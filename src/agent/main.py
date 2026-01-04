6a39fa274970290d7bed46edc70ae118a00a08f0
DannyTram0311 <dattram5@gmail.com>
2026-01-04 22:31:55 +0700

    [fix] modify java version in dockerfile

--- src/agent/main.py
+++ src/agent/main.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-IS_APPLE_SILICON = os.getenv("IS_APPLE_SILICON", True)
+IS_APPLE_SILICON = os.getenv("IS_APPLE_SILICON", False)
 
 # Redis init
 @asynccontextmanager
